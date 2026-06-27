3c7be5e085cfe239a28073cf12f43af1ebd9dd3f
Alanis Alvarez <alanisalvarez.b19@gmail.com>
2026-06-27 11:31:56 -0600

    Creacion del proyecto

---
+++ src/main/java/com/somnia/somnia/SomniaApplication.java
@@ -0,0 +1,13 @@
+package com.somnia.somnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somnia/somnia/SomniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somnia.somnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
